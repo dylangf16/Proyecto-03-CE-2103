96e004ce5ec4727b0619116ede54d7f0057be3ba
Dylan Garbanzo <dgarbanzof@estudiantec.cr>
2022-06-15 11:26:54 -0600

    Arreglos de interfaz
    Aún no funciona eliminar archivos

--- Interfaz/web-mongo/app.js
+++ Interfaz/web-mongo/app.js
@@ -1,33 +1,36 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const crypto = require('crypto');
-const mongoose = require('mongoose');
-const multer = require('multer');
-const GridFsStorage = require('multer-gridfs-storage').GridFsStorage;
-const Grid = require('gridfs-stream');
-const methodOverride = require('method-override');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const crypto = require("crypto");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const GridFsStorage = require("multer-gridfs-storage").GridFsStorage;
+const Grid = require("gridfs-stream");
+const methodOverride = require("method-override");
 
 const app = express();
 
 // Middleware
 app.use(bodyParser.json());
-app.use(methodOverride('_method'));
-app.set('view engine', 'ejs');
+app.use(methodOverride("_method"));
+app.set("view engine", "ejs");
 
 // Mongo URI
-const mongoURI = 'mongodb+srv://dgarbanzo:5Q8dgh8BBsBDOnsc@cluster0.4vwti2r.mongodb.net/?retryWrites=true&w=majority';
+const mongoURI =
+  "mongodb+srv://dgarbanzo:5Q8dgh8BBsBDOnsc@cluster0.4vwti2r.mongodb.net/?retryWrites=true&w=majority";
 
 // Create mongo connection
 const conn = mongoose.createConnection(mongoURI);
 
 // Init gfs
-let gfs;
+let gfs, gridfsBucket;
+conn.once("open", () => {
+  gridfsBucket = new mongoose.mongo.GridFSBucket(conn.db, {
+    bucketName: "uploads",
+  });
 
-conn.once('open', () => {
-  // Init stream
   gfs = Grid(conn.db, mongoose.mongo);
-  gfs.collection('uploads');
+  gfs.collection("uploads");
 });
 
 // Create storage engine
@@ -39,56 +42,56 @@ const storage = new GridFsStorage({
         if (err) {
           return reject(err);
         }
-        const filename = buf.toString('hex') + path.extname(file.originalname);
+        const filename = buf.toString("hex") + path.extname(file.originalname);
         const fileInfo = {
           filename: filename,
-          bucketName: 'uploads'
+          bucketName: "uploads",
         };
         resolve(fileInfo);
       });
     });
-  }
+  },
 });
 const upload = multer({ storage });
 
 // @route GET /
 // @desc Loads form
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   gfs.files.find().toArray((err, files) => {
     // Check if files
     if (!files || files.length === 0) {
-      res.render('index', { files: false });
+      res.render("index", { files: false });
     } else {
-      files.map(file => {
+      files.map((file) => {
         if (
-          file.contentType === 'image/jpeg' ||
-          file.contentType === 'image/png'
+          file.contentType === "image/jpeg" ||
+          file.contentType === "image/png"
         ) {
           file.isImage = true;
         } else {
           file.isImage = false;
         }
       });
-      res.render('index', { files: files });
+      res.render("index", { files: files });
     }
   });
 });
 
 // @route POST /upload
 // @desc  Uploads file to DB
-app.post('/upload', upload.single('file'), (req, res) => {
+app.post("/upload", upload.single("file"), (req, res) => {
   // res.json({ file: req.file });
-  res.redirect('/');
+  res.redirect("/");
 });
 
 // @route GET /files
 // @desc  Display all files in JSON
-app.get('/files', (req, res) => {
+app.get("/files", (req, res) => {
   gfs.files.find().toArray((err, files) => {
     // Check if files
     if (!files || files.length === 0) {
       return res.status(404).json({
-        err: 'No files exist'
+        err: "No files exist",
       });
     }
 
@@ -99,12 +102,12 @@ app.get('/files', (req, res) => {
 
 // @route GET /files/:filename
 // @desc  Display single file object
-app.get('/files/:filename', (req, res) => {
+app.get("/files/:filename", (req, res) => {
   gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
     // Check if file
     if (!file || file.length === 0) {
       return res.status(404).json({
-        err: 'No file exists'
+        err: "No file exists",
       });
     }
     // File exists
@@ -114,23 +117,22 @@ app.get('/files/:filename', (req, res) => {
 
 // @route GET /image/:filename
 // @desc Display Image
-app.get('/image/:filename', (req, res) => {
+app.get("/image/:filename", (req, res) => {
   gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
     // Check if file
     if (!file || file.length === 0) {
       return res.status(404).json({
-        err: 'No file exists'
+        err: "No file exists",
       });
     }
 
     // Check if image
-    if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
-      // Read output to browser
-      const readstream = gfs.createReadStream(file.filename);
-      readstream.pipe(res);
+    if (file.contentType === "image/jpeg" || file.contentType === "image/png") {
+      const readStream = gridfsBucket.openDownloadStream(file._id);
+      readStream.pipe(res);
     } else {
       res.status(404).json({
-        err: 'Not an image'
+        err: "Not an image",
       });
     }
   });
@@ -138,13 +140,13 @@ app.get('/image/:filename', (req, res) => {
 
 // @route DELETE /files/:id
 // @desc  Delete file
-app.delete('/files/:id', (req, res) => {
-  gfs.remove({ _id: req.params.id, root: 'uploads' }, (err, gridStore) => {
+app.delete("/files/:id", (req, res) => {
+  gfs.remove({ _id: req.params.id, root: "uploads" }, (err, gridStore) => {
     if (err) {
       return res.status(404).json({ err: err });
     }
 
-    res.redirect('/');
+    res.redirect("/");
   });
 });
 
