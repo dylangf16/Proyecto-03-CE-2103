246ab065485d6985505478a754af2ebcab74afc3
Dylan Garbanzo <dgarbanzof@estudiantec.cr>
2022-06-17 16:37:46 -0600

    Avance descargar archivo desde mongodb

--- Interfaz/web-mongo/app.js
+++ Interfaz/web-mongo/app.js
@@ -1,13 +1,12 @@
 //import React, { useState } from "react";
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require("path");
-const crypto = require("crypto");
 const mongoose = require("mongoose");
 const multer = require("multer");
 const GridFsStorage = require("multer-gridfs-storage").GridFsStorage;
 const Grid = require("gridfs-stream");
 const methodOverride = require("method-override");
+const { flushSync } = require("react-dom");
 const app = express();
 
 // Middleware
@@ -38,20 +37,13 @@ const storage = new GridFsStorage({
   url: mongoURI,
   file: (req, file) => {
     return new Promise((resolve, reject) => {
-      crypto.randomBytes(16, (err, buf) => {
-        if (err) {
-          return reject(err);
-        }
-
-        const userName = req.body?.email;
-
-        const filename = userName + buf.toString("hex") + path.extname(file.originalname);
-        const fileInfo = {
-          filename: filename,
-          bucketName: "uploads",
-        };
-        resolve(fileInfo);
-      });
+      const userName = req.body?.email;
+      const filename = userName + "_///_" + file.originalname;
+      const fileInfo = {
+        filename: filename,
+        bucketName: "uploads",
+      };
+      resolve(fileInfo);
     });
   },
 });
@@ -60,24 +52,26 @@ const upload = multer({ storage });
 // @route GET /
 // @desc Loads form
 app.get("/", (req, res) => {
-  gfs?.files.find({ filename: {"$regex" : new RegExp(`^${req.query.user}`) } }).toArray((err, files) => {
-    // Check if files
-    if (!files || files.length === 0) {
-      res.render("index", { files: false, username: req.query.user });
-    } else {
-      files.map((file) => {
-        if (
-          file.contentType === "image/jpeg" ||
-          file.contentType === "image/png"
-        ) {
-          file.isImage = true;
-        } else {
-          file.isImage = false;
-        }
-      });
-      res.render("index", { files: files, username: req.query.user });
-    }
-  });
+  gfs?.files
+    .find({ filename: { $regex: new RegExp(`^${req.query.user}`) } })
+    .toArray((err, files) => {
+      // Check if files
+      if (!files || files.length === 0) {
+        res.render("index", { files: false, username: req.query.user });
+      } else {
+        files.map((file) => {
+          if (
+            file.contentType === "image/jpeg" ||
+            file.contentType === "image/png"
+          ) {
+            file.isImage = true;
+          } else {
+            file.isImage = false;
+          }
+        });
+        res.render("index", { files: files, username: req.query.user });
+      }
+    });
 });
 
 // @route POST /upload
@@ -141,16 +135,26 @@ app.get("/image/:filename", (req, res) => {
   });
 });
 
-// @route DELETE /files/:id
-// @desc  Delete file
-exports.deleteFileByFilename = async (req, res, next) => {
-  const file = await gfs.files.findOne({ filename: req.params.filename });
-  const gsfb = new mongoose.mongo.GridFSBucket(conn.db, { bucketName: 'uploads' });
-  gsfb.delete(file._id, function (err, gridStore) {
-    if (err) return next(err);
-    res.status(200).end();
+app.get("/download", (req, res) => {
+  gfs.files.findOne({ _id: req.params._id}, (err, file) => {
+    if (err) return handleError(err);
+    if (!file) return res.send("Error on the database looking for the file.");
+    res.set("Content-Type", file.contentType);
+    res.set(
+      "Content-Disposition",
+      'attachment; filename="' + file.filename + '"'
+    );
+
+    var readstream = gfs.createReadStream({
+      _id: req.params._id
+    });
+
+    readstream.on("error", function (err) {
+      res.end();
+    });
+    readstream.pipe(res);
   });
-};
+});
 
 const port = 5000;
 
